a71d8853d1cdbbc04fea4d5fab4a054f9101fe24
dleovl <95427722+dleovl@users.noreply.github.com>
2024-02-17 16:03:41 -0500

    better than any nightwind tweak
    and icraze

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# set to device ip
+ip="10.0.0.18"
+# set to device username
+user="mobile"
+
+rm derootifier-whitename.deb
+rm ./derootifier-whitename/.DS_Store
+rm ./derootifier-whitename/DEBIAN/.DS_Store
+dpkg-deb --build derootifier-whitename
+echo attempting to remove "$user"@"$ip":/var/mobile/Documents/derootifier-whitename.deb
+echo cancel the script with ctrl + c if this would be malicious
+ssh "$user"@"$ip" 'rm /var/mobile/Documents/derootifier-whitename.deb'
+echo starting scp transfer of derootifier-whitename.deb
+scp derootifier-whitename.deb "$user"@"$ip":/var/mobile/Documents/
---
+++ readme.md
@@ -0,0 +1,10 @@
+# derootifier-whitename
+whitename-friendly installer for derootifier since opa wants to drop literally out of nowhere and the whitenames come swarming in like bees
+
+### wtf does it do
+it just Depends on all the Derootifier dependencies in control and opens the Derootifier ipa into TrollStore with the postinst and uikittools. thats it
+
+## credit!!!
+haiiiii ty joshuah345 and donato-fiore ur da best ong frfrfr
+
+also shoutout to uikittools its neat
