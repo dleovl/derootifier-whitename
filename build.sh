b7879e4788c6e96470d2184b8e8f5b1902150cee
dleovl <95427722+dleovl@users.noreply.github.com>
2024-02-17 16:41:51 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ ip="10.0.0.18"
 # set to device username
 user="mobile"
 
-rm derootifier-whitename.deb
+rm ./derootifier-whitename.deb
 rm ./derootifier-whitename/.DS_Store
 rm ./derootifier-whitename/DEBIAN/.DS_Store
 dpkg-deb --build derootifier-whitename
@@ -13,4 +13,4 @@ echo attempting to remove "$user"@"$ip":/var/mobile/Documents/derootifier-whiten
 echo cancel the script with ctrl + c if this would be malicious
 ssh "$user"@"$ip" 'rm /var/mobile/Documents/derootifier-whitename.deb'
 echo starting scp transfer of derootifier-whitename.deb
-scp derootifier-whitename.deb "$user"@"$ip":/var/mobile/Documents/
+scp ./derootifier-whitename.deb "$user"@"$ip":/var/mobile/Documents/
